b9769e583ab614d4c1f8f3073c7061e46e28b55b
MasonTang <masontang@aol.com>
2019-01-08 06:58:40 -0600

    initial

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
+exports.Test_DATABASE_URL = process.env.Test_DATABASE_URL || 'mongodb://localhost/test-blog-app';
 exports.PORT = process.env.PORT || 8080;
---
+++ test/test-blog-integration.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const expect = chai.expect;
+
+
+const {BlogPost} = require('../models')
